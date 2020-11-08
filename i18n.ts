9f976cca05ce1a0d7bcea642aaabead3ec36cd22
msirghi <mihail.sirghi@orange.md>
2020-11-08 15:16:38 +0200

    STP-13 Transformed js files to ts

--- i18n.ts
+++ i18n.ts
@@ -1,3 +1,4 @@
+/* eslint-disable */
 const NextI18Next = require('next-i18next').default;
 const { localeSubpaths } = require('next/config').default().publicRuntimeConfig;
 const path = require('path');

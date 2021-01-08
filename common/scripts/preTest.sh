f1cca673d0b333b2f4388822c1d4f212fab85c80
= <=>
2021-01-08 22:29:25 +0200

    Dev: added script for pre test

---
+++ common/scripts/preTest.sh
@@ -0,0 +1,10 @@
+flag=$1
+file=tsconfig.json
+
+echo "flag for isolated modules: $flag"
+
+cd ..
+cd ..
+perl -pi -e "s/\"isolatedModules\": false/\"isolatedModules\": $1/g" $file
+
+echo "isolated modules flag changed. Current value: $flag"

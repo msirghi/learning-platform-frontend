e94bbf29b2b85273e5e8e88a235fe1103de5d522
= <=>
2021-01-08 10:29:17 +0200

    Dev: commented pre commit hook

--- common/preStartHook.ts
+++ common/preStartHook.ts
@@ -1,16 +1,16 @@
-/* eslint-disable */
-// @ts-nocheck
-const fs = require('fs');
-const path = require('path');
+// /* eslint-disable */
+// // @ts-nocheck
+// const fs = require('fs');
+// const path = require('path');
 
-fs.readFile(path.resolve(__dirname, '../tsconfig.json'), 'utf-8', (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  const result = data.replace('"isolatedModules": false', '"isolatedModules": true');
-  fs.writeFile(path.resolve(__dirname, '../tsconfig.json'), result, 'utf8', (err) => {
-    if (err) return console.log(err);
-    console.log('Changed flag to false for isloated modules.');
-  });
-});
+// fs.readFile(path.resolve(__dirname, '../tsconfig.json'), 'utf-8', (err, data) => {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+//   const result = data.replace('"isolatedModules": false', '"isolatedModules": true');
+//   fs.writeFile(path.resolve(__dirname, '../tsconfig.json'), result, 'utf8', (err) => {
+//     if (err) return console.log(err);
+//     console.log('Changed flag to false for isloated modules.');
+//   });
+// });
